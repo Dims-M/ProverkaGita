f538adffac1b8bcf5158b7bba4d3a8b0a3567d52
Dmitriy <asdg@mail.ru>
2016-09-23 23:17:40 +0300

    Первый коммитgit status

---
+++ src/ru/gitProet/Animal.java
@@ -0,0 +1,17 @@
+package ru.gitProet;
+
+/**
+ * Created by Администратор on 23.09.2016.
+ */
+public class Animal {
+
+    String name;
+    String svet;
+    String poroda;
+
+    int age;
+    int vozrast;
+
+    boolean pol = false; // епо умолчанию женский.
+
+}
---
+++ src/ru/gitProet/Mail.java
@@ -0,0 +1,14 @@
+package ru.gitProet;
+
+/**
+ * Created by Администратор on 23.09.2016.
+ */
+public class Mail {
+
+    public static void main(String[] args) {
+
+
+
+
+    } // конец класса Main
+}
