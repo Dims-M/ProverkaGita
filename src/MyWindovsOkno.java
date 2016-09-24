7060d3864ce7009b991dbe96f38acd2f1dca4a3f
Dmitriy <asdg@mail.ru>
2016-09-24 12:17:54 +0300

    Версия коммита 6 из IDI Идея Добавили Окно sving

---
+++ src/MyWindovsOkno.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Администратор on 24.09.2016.
+ */
+public class MyWindovsOkno  extends JFrame{
+    public static void main(String[] args) {
+
+
+    } // конец метода майн
+}
